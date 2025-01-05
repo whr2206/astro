c27efcffa2d05a5658f824ca1df0d144422f500a
Johnny <whr2206@hotmail.com>
2025-01-04 20:34:05 -0500

    ran astro build in terminal

---
+++ .netlify/build/chunks/astro/server_BPLR8myI.mjs
@@ -0,0 +1,382 @@
+import 'kleur/colors';
+import { escape } from 'html-escaper';
+import 'clsx';
+import { decodeBase64 } from '@oslojs/encoding';
+import 'cssesc';
+
+const ASTRO_VERSION = "5.1.2";
+const NOOP_MIDDLEWARE_HEADER = "X-Astro-Noop";
+
+const InvalidComponentArgs = {
+  name: "InvalidComponentArgs",
+  title: "Invalid component arguments.",
+  message: (name) => `Invalid arguments passed to${name ? ` <${name}>` : ""} component.`,
+  hint: "Astro components cannot be rendered directly via function call, such as `Component()` or `{items.map(Component)}`."
+};
+const AstroGlobUsedOutside = {
+  name: "AstroGlobUsedOutside",
+  title: "Astro.glob() used outside of an Astro file.",
+  message: (globStr) => `\`Astro.glob(${globStr})\` can only be used in \`.astro\` files. \`import.meta.glob(${globStr})\` can be used instead to achieve a similar result.`,
+  hint: "See Vite's documentation on `import.meta.glob` for more information: https://vite.dev/guide/features.html#glob-import"
+};
+const AstroGlobNoMatch = {
+  name: "AstroGlobNoMatch",
+  title: "Astro.glob() did not match any files.",
+  message: (globStr) => `\`Astro.glob(${globStr})\` did not return any matching files.`,
+  hint: "Check the pattern for typos."
+};
+
+function normalizeLF(code) {
+  return code.replace(/\r\n|\r(?!\n)|\n/g, "\n");
+}
+
+function codeFrame(src, loc) {
+  if (!loc || loc.line === void 0 || loc.column === void 0) {
+    return "";
+  }
+  const lines = normalizeLF(src).split("\n").map((ln) => ln.replace(/\t/g, "  "));
+  const visibleLines = [];
+  for (let n = -2; n <= 2; n++) {
+    if (lines[loc.line + n]) visibleLines.push(loc.line + n);
+  }
+  let gutterWidth = 0;
+  for (const lineNo of visibleLines) {
+    let w = `> ${lineNo}`;
+    if (w.length > gutterWidth) gutterWidth = w.length;
+  }
+  let output = "";
+  for (const lineNo of visibleLines) {
+    const isFocusedLine = lineNo === loc.line - 1;
+    output += isFocusedLine ? "> " : "  ";
+    output += `${lineNo + 1} | ${lines[lineNo]}
+`;
+    if (isFocusedLine)
+      output += `${Array.from({ length: gutterWidth }).join(" ")}  | ${Array.from({
+        length: loc.column
+      }).join(" ")}^
+`;
+  }
+  return output;
+}
+
+class AstroError extends Error {
+  loc;
+  title;
+  hint;
+  frame;
+  type = "AstroError";
+  constructor(props, options) {
+    const { name, title, message, stack, location, hint, frame } = props;
+    super(message, options);
+    this.title = title;
+    this.name = name;
+    if (message) this.message = message;
+    this.stack = stack ? stack : this.stack;
+    this.loc = location;
+    this.hint = hint;
+    this.frame = frame;
+  }
+  setLocation(location) {
+    this.loc = location;
+  }
+  setName(name) {
+    this.name = name;
+  }
+  setMessage(message) {
+    this.message = message;
+  }
+  setHint(hint) {
+    this.hint = hint;
+  }
+  setFrame(source, location) {
+    this.frame = codeFrame(source, location);
+  }
+  static is(err) {
+    return err.type === "AstroError";
+  }
+}
+
+function validateArgs(args) {
+  if (args.length !== 3) return false;
+  if (!args[0] || typeof args[0] !== "object") return false;
+  return true;
+}
+function baseCreateComponent(cb, moduleId, propagation) {
+  const name = moduleId?.split("/").pop()?.replace(".astro", "") ?? "";
+  const fn = (...args) => {
+    if (!validateArgs(args)) {
+      throw new AstroError({
+        ...InvalidComponentArgs,
+        message: InvalidComponentArgs.message(name)
+      });
+    }
+    return cb(...args);
+  };
+  Object.defineProperty(fn, "name", { value: name, writable: false });
+  fn.isAstroComponentFactory = true;
+  fn.moduleId = moduleId;
+  fn.propagation = propagation;
+  return fn;
+}
+function createComponentWithOptions(opts) {
+  const cb = baseCreateComponent(opts.factory, opts.moduleId, opts.propagation);
+  return cb;
+}
+function createComponent(arg1, moduleId, propagation) {
+  if (typeof arg1 === "function") {
+    return baseCreateComponent(arg1, moduleId, propagation);
+  } else {
+    return createComponentWithOptions(arg1);
+  }
+}
+
+function createAstroGlobFn() {
+  const globHandler = (importMetaGlobResult) => {
+    console.warn(`Astro.glob is deprecated and will be removed in a future major version of Astro.
+Use import.meta.glob instead: https://vitejs.dev/guide/features.html#glob-import`);
+    if (typeof importMetaGlobResult === "string") {
+      throw new AstroError({
+        ...AstroGlobUsedOutside,
+        message: AstroGlobUsedOutside.message(JSON.stringify(importMetaGlobResult))
+      });
+    }
+    let allEntries = [...Object.values(importMetaGlobResult)];
+    if (allEntries.length === 0) {
+      throw new AstroError({
+        ...AstroGlobNoMatch,
+        message: AstroGlobNoMatch.message(JSON.stringify(importMetaGlobResult))
+      });
+    }
+    return Promise.all(allEntries.map((fn) => fn()));
+  };
+  return globHandler;
+}
+function createAstro(site) {
+  return {
+    // TODO: this is no longer necessary for `Astro.site`
+    // but it somehow allows working around caching issues in content collections for some tests
+    site: void 0,
+    generator: `Astro v${ASTRO_VERSION}`,
+    glob: createAstroGlobFn()
+  };
+}
+
+function isPromise(value) {
+  return !!value && typeof value === "object" && "then" in value && typeof value.then === "function";
+}
+
+const escapeHTML = escape;
+class HTMLString extends String {
+  get [Symbol.toStringTag]() {
+    return "HTMLString";
+  }
+}
+const markHTMLString = (value) => {
+  if (value instanceof HTMLString) {
+    return value;
+  }
+  if (typeof value === "string") {
+    return new HTMLString(value);
+  }
+  return value;
+};
+function isHTMLString(value) {
+  return Object.prototype.toString.call(value) === "[object HTMLString]";
+}
+
+const RenderInstructionSymbol = Symbol.for("astro:render");
+function createRenderInstruction(instruction) {
+  return Object.defineProperty(instruction, RenderInstructionSymbol, {
+    value: true
+  });
+}
+
+const htmlBooleanAttributes = /^(?:allowfullscreen|async|autofocus|autoplay|checked|controls|default|defer|disabled|disablepictureinpicture|disableremoteplayback|formnovalidate|hidden|inert|loop|nomodule|novalidate|open|playsinline|readonly|required|reversed|scoped|seamless|selected|itemscope)$/i;
+const AMPERSAND_REGEX = /&/g;
+const DOUBLE_QUOTE_REGEX = /"/g;
+const STATIC_DIRECTIVES = /* @__PURE__ */ new Set(["set:html", "set:text"]);
+const toAttributeString = (value, shouldEscape = true) => shouldEscape ? String(value).replace(AMPERSAND_REGEX, "&#38;").replace(DOUBLE_QUOTE_REGEX, "&#34;") : value;
+function addAttribute(value, key, shouldEscape = true) {
+  if (value == null) {
+    return "";
+  }
+  if (STATIC_DIRECTIVES.has(key)) {
+    console.warn(`[astro] The "${key}" directive cannot be applied dynamically at runtime. It will not be rendered as an attribute.
+
+Make sure to use the static attribute syntax (\`${key}={value}\`) instead of the dynamic spread syntax (\`{...{ "${key}": value }}\`).`);
+    return "";
+  }
+  if (typeof value === "string" && value.includes("&") && isHttpUrl(value)) {
+    return markHTMLString(` ${key}="${toAttributeString(value, false)}"`);
+  }
+  if (htmlBooleanAttributes.test(key)) {
+    return markHTMLString(value ? ` ${key}` : "");
+  }
+  if (value === "") {
+    return markHTMLString(` ${key}`);
+  }
+  return markHTMLString(` ${key}="${toAttributeString(value, shouldEscape)}"`);
+}
+const noop = () => {
+};
+class BufferedRenderer {
+  chunks = [];
+  renderPromise;
+  destination;
+  constructor(bufferRenderFunction) {
+    this.renderPromise = bufferRenderFunction(this);
+    Promise.resolve(this.renderPromise).catch(noop);
+  }
+  write(chunk) {
+    if (this.destination) {
+      this.destination.write(chunk);
+    } else {
+      this.chunks.push(chunk);
+    }
+  }
+  async renderToFinalDestination(destination) {
+    for (const chunk of this.chunks) {
+      destination.write(chunk);
+    }
+    this.destination = destination;
+    await this.renderPromise;
+  }
+}
+function renderToBufferDestination(bufferRenderFunction) {
+  const renderer = new BufferedRenderer(bufferRenderFunction);
+  return renderer;
+}
+typeof process !== "undefined" && Object.prototype.toString.call(process) === "[object process]";
+const VALID_PROTOCOLS = ["http:", "https:"];
+function isHttpUrl(url) {
+  try {
+    const parsedUrl = new URL(url);
+    return VALID_PROTOCOLS.includes(parsedUrl.protocol);
+  } catch {
+    return false;
+  }
+}
+
+function renderHead() {
+  return createRenderInstruction({ type: "head" });
+}
+
+const renderTemplateResultSym = Symbol.for("astro.renderTemplateResult");
+class RenderTemplateResult {
+  [renderTemplateResultSym] = true;
+  htmlParts;
+  expressions;
+  error;
+  constructor(htmlParts, expressions) {
+    this.htmlParts = htmlParts;
+    this.error = void 0;
+    this.expressions = expressions.map((expression) => {
+      if (isPromise(expression)) {
+        return Promise.resolve(expression).catch((err) => {
+          if (!this.error) {
+            this.error = err;
+            throw err;
+          }
+        });
+      }
+      return expression;
+    });
+  }
+  async render(destination) {
+    const expRenders = this.expressions.map((exp) => {
+      return renderToBufferDestination((bufferDestination) => {
+        if (exp || exp === 0) {
+          return renderChild(bufferDestination, exp);
+        }
+      });
+    });
+    for (let i = 0; i < this.htmlParts.length; i++) {
+      const html = this.htmlParts[i];
+      const expRender = expRenders[i];
+      destination.write(markHTMLString(html));
+      if (expRender) {
+        await expRender.renderToFinalDestination(destination);
+      }
+    }
+  }
+}
+function isRenderTemplateResult(obj) {
+  return typeof obj === "object" && obj !== null && !!obj[renderTemplateResultSym];
+}
+function renderTemplate(htmlParts, ...expressions) {
+  return new RenderTemplateResult(htmlParts, expressions);
+}
+
+const slotString = Symbol.for("astro:slot-string");
+class SlotString extends HTMLString {
+  instructions;
+  [slotString];
+  constructor(content, instructions) {
+    super(content);
+    this.instructions = instructions;
+    this[slotString] = true;
+  }
+}
+
+new TextEncoder();
+new TextDecoder();
+function isRenderInstance(obj) {
+  return !!obj && typeof obj === "object" && "render" in obj && typeof obj.render === "function";
+}
+
+async function renderChild(destination, child) {
+  if (isPromise(child)) {
+    child = await child;
+  }
+  if (child instanceof SlotString) {
+    destination.write(child);
+  } else if (isHTMLString(child)) {
+    destination.write(child);
+  } else if (Array.isArray(child)) {
+    const childRenders = child.map((c) => {
+      return renderToBufferDestination((bufferDestination) => {
+        return renderChild(bufferDestination, c);
+      });
+    });
+    for (const childRender of childRenders) {
+      if (!childRender) continue;
+      await childRender.renderToFinalDestination(destination);
+    }
+  } else if (typeof child === "function") {
+    await renderChild(destination, child());
+  } else if (typeof child === "string") {
+    destination.write(markHTMLString(escapeHTML(child)));
+  } else if (!child && child !== 0) ; else if (isRenderInstance(child)) {
+    await child.render(destination);
+  } else if (isRenderTemplateResult(child)) {
+    await child.render(destination);
+  } else if (isAstroComponentInstance(child)) {
+    await child.render(destination);
+  } else if (ArrayBuffer.isView(child)) {
+    destination.write(child);
+  } else if (typeof child === "object" && (Symbol.asyncIterator in child || Symbol.iterator in child)) {
+    for await (const value of child) {
+      await renderChild(destination, value);
+    }
+  } else {
+    destination.write(child);
+  }
+}
+
+const astroComponentInstanceSym = Symbol.for("astro.componentInstance");
+function isAstroComponentInstance(obj) {
+  return typeof obj === "object" && obj !== null && !!obj[astroComponentInstanceSym];
+}
+
+const ALGORITHM = "AES-GCM";
+async function decodeKey(encoded) {
+  const bytes = decodeBase64(encoded);
+  return crypto.subtle.importKey("raw", bytes, ALGORITHM, true, ["encrypt", "decrypt"]);
+}
+new TextEncoder();
+new TextDecoder();
+
+"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_".split("").reduce((v, c) => (v[c.charCodeAt(0)] = c, v), []);
+"-0123456789_".split("").reduce((v, c) => (v[c.charCodeAt(0)] = c, v), []);
+
+export { NOOP_MIDDLEWARE_HEADER as N, addAttribute as a, renderHead as b, createComponent as c, createAstro as d, decodeKey as e, renderTemplate as r };
