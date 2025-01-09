5a804afa7cf67747a304179f57995df0eafdac96
Johnny <whr2206@hotmail.com>
2025-01-08 22:22:41 -0500

    added rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
+  site: "https://astro-test-whr.netlify.app",
+  adapter: netlify(),
+  // Add RSS feed configuration here
+  // ...
 });
+
