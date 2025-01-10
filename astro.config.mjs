a39e42dbf44bef262e6bbe2a6693c7b5fa8e97d3
Johnny <whr2206@hotmail.com>
2025-01-09 19:13:59 -0500

    added preact and test greeting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
+import preact from '@astrojs/preact';
+
 export default defineConfig({
   site: "https://astro-test-whr.netlify.app",
-  adapter: netlify(),
+
   // Add RSS feed configuration here
   // ...
-});
+  adapter: netlify(),
 
+  integrations: [preact()]
+});
